c39851b61c5f0f5a53d2c515681521371ca43553
ojg <withyouatw@gmail.com>
2023-10-24 17:36:48 +0900

    .

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,12 @@
 (function() {
     var searchTerm = decodeURIComponent(window.location.search.split('?q=')[1]);
+
+    console.log("searchTerm : ", searchTerm)
     if (searchTerm) {
         searchTerm = searchTerm.toLowerCase();
         var posts = document.querySelectorAll('.post-title');
+
+        console.log("posts : ", posts);
         posts.forEach(function(post) {
             var title = post.innerText.toLowerCase();
             if (title.includes(searchTerm)) {
