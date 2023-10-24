ef64e37f0913ff51b1d06f91c5a8c5f0833789a5
ojg <withyouatw@gmail.com>
2023-10-24 17:53:59 +0900

    .

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,41 +1,41 @@
-// (function() {
-//     var searchTerm = decodeURIComponent(window.location.search.split('?q=')[1]);
-
-//     console.log("searchTerm : ", searchTerm)
-//     if (searchTerm) {
-//         searchTerm = searchTerm.toLowerCase();
-//         var posts = document.querySelectorAll('.post-title');
-
-//         console.log("posts : ", posts);
-//         posts.forEach(function(post) {
-//             var title = post.innerText.toLowerCase();
-//             if (title.includes(searchTerm)) {
-//                 post.closest('.post').style.display = 'block';
-//             } else {
-//                 post.closest('.post').style.display = 'none';
-//             }
-//         });
-//     }
-// })();
-
-
 (function() {
     var searchTerm = decodeURIComponent(window.location.search.split('?q=')[1]);
 
-    console.log("searchTerm : ", searchTerm);
+    console.log("searchTerm : ", searchTerm)
     if (searchTerm) {
         searchTerm = searchTerm.toLowerCase();
-        var posts = document.querySelectorAll('.post');
+        var posts = document.querySelectorAll('.post-title');
 
         console.log("posts : ", posts);
         posts.forEach(function(post) {
-            var title = post.querySelector('.post-title a').innerText.toLowerCase();
-            var content = post.querySelector('.post-content p').innerText.toLowerCase();
-            if (title.includes(searchTerm) || content.includes(searchTerm)) {
-                post.style.display = 'block';
+            var title = post.innerText.toLowerCase();
+            if (title.includes(searchTerm)) {
+                post.closest('.post').style.display = 'block';
             } else {
-                post.style.display = 'none';
+                post.closest('.post').style.display = 'none';
             }
         });
     }
 })();
+
+
+// (function() {
+//     var searchTerm = decodeURIComponent(window.location.search.split('?q=')[1]);
+
+//     console.log("searchTerm : ", searchTerm);
+//     if (searchTerm) {
+//         searchTerm = searchTerm.toLowerCase();
+//         var posts = document.querySelectorAll('.post');
+
+//         console.log("posts : ", posts);
+//         posts.forEach(function(post) {
+//             var title = post.querySelector('.post-title a').innerText.toLowerCase();
+//             var content = post.querySelector('.post-content p').innerText.toLowerCase();
+//             if (title.includes(searchTerm) || content.includes(searchTerm)) {
+//                 post.style.display = 'block';
+//             } else {
+//                 post.style.display = 'none';
+//             }
+//         });
+//     }
+// })();
